808281a7c665cd291fc53372f8e0cb6a2b3c835b
Jacob Howell <jhowell45@googlemail.com>
2024-08-27 00:49:57 +0100

    Added sprint.

--- player.gd
+++ player.gd
@@ -5,6 +5,8 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export var sprint_multiplier = 2.5
+
 var target_velocity = Vector3.ZERO
 
 @onready var animator: AnimationPlayer = %AnimationPlayer
@@ -12,6 +14,10 @@ var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
+	var local_speed = speed
+	if Input.is_action_pressed("sprint"):
+		print("pressed")
+		local_speed *= sprint_multiplier
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 	if Input.is_action_pressed("left"):
@@ -25,8 +31,8 @@ func _physics_process(delta):
 		$Pivot.basis = Basis.looking_at(direction)
 	
 	# Ground Velocity
-	target_velocity.x = direction.x * speed * delta
-	target_velocity.z = direction.z * speed * delta
+	target_velocity.x = direction.x * local_speed * delta
+	target_velocity.z = direction.z * local_speed * delta
 
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
